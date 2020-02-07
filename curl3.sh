804f79f51a880d68f6de999c7c29a6b3c8697781
Sahil Joseph <sahiljoseph20@gmail.com>
2020-02-07 15:38:01 +0530

    Update curl3.sh

--- curl3.sh
+++ curl3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 for i in {1..10}; do
-URL="http://172.16.0.240:8080/core/healthcheck/"
+URL="http://<ip>:<port>/core/healthcheck/"
 HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}"  $URL)
 HTTP_STATUS=$(echo $HTTP_RESPONSE | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
 if [  "$HTTP_STATUS"="200" ]; then
