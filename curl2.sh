95ba0269b72135e11aaab8109d1cf526af8e19ee
Sahil Joseph <sahiljoseph20@gmail.com>
2020-02-07 15:41:10 +0530

    Update curl2.sh

--- curl2.sh
+++ curl2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -B                 
 for i in {1..10}; do
-    curl -Is 'http://172.16.0.240:8080/core/healthcheck/ -L '
-    #curl -Is http://172.16.0.240:8080/core/healthcheck/ -L
+    curl -Is 'http://<ip>:<port>/core/healthcheck/ -L '
+    #curl -Is 'http://<host_ip>/core/healthcheck/ -L 
 done
